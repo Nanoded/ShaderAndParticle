0e5bbca97049753e36b2c653e3eef884a4ce7c91
Sergey Sochinskiy <empty.headed21@gmail.com>
2022-02-13 00:24:41 +0300

    init

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _shootEffect;
+    [SerializeField] private ParticleSystem _hitEffect;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        StartShoot();
+    }
+
+    private void StartShoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            {
+                _animator.SetTrigger("Shoot");
+                _shootEffect.Play();
+                CheckHit();
+            }
+        }
+    }
+
+    private void CheckHit()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(_shootEffect.transform.position, transform.forward, out hit))
+        {
+            _hitEffect.transform.position = hit.point;
+            _hitEffect.Play();
+        }
+    }
+}
